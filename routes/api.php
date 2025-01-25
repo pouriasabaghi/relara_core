487dc7766228c018de94b9973aa99a685b485d46
pouriasabaghi <pouriasabaghi@gmail.com>
2025-01-25 20:48:52 +0330

    chore: change admin route and add prefix

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,19 @@ Route::prefix('v1')->group(function () {
     Route::post('/login', [App\Http\Controllers\api\v1\UserAuthController::class, 'login']);
     Route::post('/logout', [App\Http\Controllers\api\v1\UserAuthController::class, 'logout'])->middleware('auth:sanctum');
 
-    Route::middleware('auth:admin')->group(function () {
-        Route::apiResource('/users', App\Http\Controllers\api\v1\UserController::class)->middleware('auth:sanctum');
+    Route::group([], function () {
+        Route::get('categories', [App\Http\Controllers\api\v1\CategoryController::class, 'index']);
+    });
+
+    Route::prefix('admin')->middleware('auth:admin')->group(function () {
+        Route::apiResource('users', App\Http\Controllers\api\v1\UserController::class)->middleware('auth:sanctum');
         Route::get('categories/all', [App\Http\Controllers\api\v1\CategoryController::class, 'allCategories']);
         Route::apiResource('categories', App\Http\Controllers\api\v1\CategoryController::class);
         Route::apiResource('attributes', App\Http\Controllers\api\v1\AttributeController::class);
         Route::apiResource('attribute-values', App\Http\Controllers\api\v1\AttributeValueController::class);
         Route::apiResource('products', App\Http\Controllers\api\v1\ProductController::class);
         Route::apiResource('product-variants', App\Http\Controllers\api\v1\ProductVariantController::class);
-        Route::post("/upload/image", [App\Http\Controllers\api\v1\ImageController::class, 'store']);
+        Route::post("upload/image", [App\Http\Controllers\api\v1\ImageController::class, 'store']);
         Route::apiResource('discount-codes', App\Http\Controllers\api\v1\DiscountCodeController::class);
     });
 });
